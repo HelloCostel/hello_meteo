0c682ca989912ca417b91ade53f46aeb18330d10
HelloCostel <me@hellocostel.com>
2025-08-11 22:07:20 +0200

    Add hour report to sunrise/sunset representation

--- src/components/SunPosition.jsx
+++ src/components/SunPosition.jsx
@@ -17,23 +17,29 @@ export default function SunPosition({ activeTime, sunrise, sunset }) {
         left: `${calcOffset()}%`
     }
 
+    const calcTimeTo = () => {
+        if (activeTime >= sunrise && activeTime <= sunset) {
+            return `${sunset - activeTime} hours to sunset`
+        }
+        else if (activeTime < sunrise) {
+            return `${sunrise - activeTime} hours to sunrise`
+        }
+        else if (activeTime > sunset) {
+            return `${(24 - activeTime) + sunrise} hours to sunrise`
+        }
+    }
+
     return (
         <>
             <div className='w-[90vw] h-[50px] relative left-[50%] flex justify-center items-center transform -translate-x-1/2'>
-                <div className='w-full h-[2px] rounded-full bg-sky-200'></div>
+                <div className='w-full h-[2px] rounded-full bg-gray-400'></div>
                 <div className='absolute h-[45px] w-[45px] rounded-full sun-gradient transform -translate-x-1/2' style={offset}></div>
+            </div>
+            <div className='w-[90vw] relative flex justify-between items-center left-[50%] transform -translate-x-1/2'>
+                    <div className='font-bold text-l text-gray-400 '>{sunrise}</div>
+                    <div className='font-bold text-l text-gray-400 '>{calcTimeTo()}</div>
+                    <div className='font-bold text-l text-gray-400 '>{sunset}</div>
             </div>
         </>
     )
 }
-
-// .container {
-//     position: relative;
-//     transform: translateX(-50%);
-//     left: 50%;
-//     width: 90vw;
-//     height: 50px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-// }
