886915ef7bd391df37a0298bfaf8d4f00c9fd26f
HelloCostel <me@hellocostel.com>
2025-08-19 17:33:49 +0200

    Animate compass on wind-direction change

--- src/components/Wind.jsx
+++ src/components/Wind.jsx
@@ -1,19 +1,26 @@
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 import compassDirection from '../assets/compass_direction.svg'
 import compass from '../assets/compass.svg'
 
 
-export default function Wind({ speed, direction}) {
+export default function Wind({ speed, direction }) {
+    const directionRef = useRef(null)
 
-    //direction value represent where wind is coming from. To represent wind-direction sum 180deg to value.
-    const compassRotation = {
-        transform: `rotate(${direction + 180}deg)`,
-    }
+    useGSAP(() => {
+        gsap.to(directionRef.current, {
+            rotation: direction + 180,
+            duration: 0.8,
+            ease: 'power2.out'
+        })
+    }, [direction])
 
     return (
         <div className='w-full h-full flex flex-col items-center justify-center'>
-            <img className='absolute h-11/12 w-11/12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={compass} />
+            <img className='absolute h-11/12 w-11/12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={compass} alt="Bussola" />
             <div className='text-sm font-bold'>Wind<br /><span>{speed} Km/h</span></div>
-            <img className='absolute' src={compassDirection} style={compassRotation}/>
+            <img ref={directionRef} className='absolute' src={compassDirection} alt="Direzione del vento" />
         </div>
     )
 }
